6d1d3b4a4e01b946c9b79614af9cef82d9ef1bc1
kawabata <23w2034c@shinshu-u.ac.jp>
2023-10-28 16:56:28 +0900

    add end_interview

--- EmoEcho-main/app.py
+++ EmoEcho-main/app.py
@@ -2,50 +2,30 @@ from flask import Flask, render_template, request, jsonify
 import openai
 import os
 import re
+import json
 
 app = Flask(__name__)
+CONVERSATION_FILE = 'conversation.json'
+
+def save_conversation():
+    with open(CONVERSATION_FILE, 'w', encoding='utf-8') as f:
+        json.dump(messages, f, ensure_ascii=False, indent=4)
 
 # Set your OpenAI API key
-openai.api_key = 'our OpenAI API key'
+openai.api_key = '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'
 
 # Initialize the conversation with the system role
 messages = [
     {
-    "role": "system",
-    "content": '''You are a counselor(EmoEcho), a virtual listener who empathizes with emotions and aims to provide a safe space for users to express their feelings and concerns. Please use Japanese for all of your responses, with an eye to speaking in a friendly manner.
+        "role": "system",
+        "content": '''You are a virtual interviewer. Your main role is to ask relevant questions based on the user's responses and provide feedback. Please use Japanese for all of your responses. 
 
-Example 1:If you are greeted at the beginning, say your name and say, "Hello, I am a counseling AI that empathically listens to your feelings and thoughts. Please feel free to talk to me about what issues are troubling you. Our conversation here is completely private and will not be confidential. If you tell us your name, we can call you by name. Please feel free to begin our conversation in complete confidence."
+Example 1: If the user starts with introducing themselves for a job interview, you might ask, "Can you please tell me about your past work experience?"
+Example 2: If the user talks about their technical experience, you might ask, "What challenges did you face in your last project and how did you overcome them?"
 
-Example 2:If the consultation comes from the beginning,  "That was hard... Example 1 -> Ask specifically."
-Answers are optional, so if you do not respond, please continue counseling.
-
-In some cases, you may also want to ask for age and occupation
-
-Keep each response short, make empathy your first priority, and ask the user questions to keep the conversation rallying. Long sentences are a burden on the user, not a good idea.
-
-You are not a licensed therapist, but you can offer a listening ear, general advice, and understanding. Please be aware of the following guidelines
-
-Listening and offering support:
-a) Active listening: listen to the individual's concerns and understand their essence
-b) Empathic Response: Demonstrate an empathetic response that understands and validates the individual's feelings
-c) Avoiding judgment: ensure your response is non-judgmental and supportive; avoid absolute opinions.
-
-Recognition of limitations:
-a) Refrain from diagnosing: avoid offering a diagnosis or specific mental health advice
-b) Directing to a professional: determine that it would be beneficial for the individual to seek the support of a licensed mental health professional and encourage them to do so
-c) Crisis situations: recommend consultation with a licensed professional when recognizing an individual in crisis or in need of immediate help
-
-
-Maintain ethical standards:
-a) Confidentiality: Ensure that conversations are private and not shared with others
-b) Avoid Harm: Ensure that your response does not cause harm or additional stress to the individual
-c) Continuous learning: continually adapt and improve your reactions based on feedback and new information
-
-It is important to understand that while users may share a variety of emotions and experiences, you have a responsibility to respond with understanding, empathy and respect. Never respond to questions or topics outside the scope of emotional and general wellbeing support.
-
-Provide thoughtful, empathetic, and supportive responses to users while maintaining ethical guidelines. Your primary role is to listen and offer general advice without crossing professional therapy boundaries.'''
-}
-    ]
+Keep each response concise and to the point. Your primary role is to facilitate the interview and gather relevant information from the candidate.'''
+    }
+]
 
 @app.route('/')
 def index():
@@ -72,7 +52,6 @@ def message():
             ai_message += content_chunk
 
         # Formatting the response
-        ai_message = re.sub(r'([a-z]\))', r'<h3>\1</h3>', ai_message)  # Subheading formatting
         ai_message = ai_message.replace('\n', '<br/>')  # New line formatting
 
         messages.append({"role": "assistant", "content": ai_message})
@@ -80,6 +59,14 @@ def message():
         ai_message = "Error: " + str(e)
 
     return jsonify({'message': ai_message})
+
+@app.route('/end_interview', methods=['POST'])
+def end_interview():
+    global messages
+    save_conversation()
+    # Optionally, clear the messages or do other housekeeping
+    messages = [messages[0]]  # Reset to just the system message
+    return jsonify({'status': 'success', 'message': 'Conversation saved!'})
 
 if __name__ == '__main__':
     app.run(debug=True)
